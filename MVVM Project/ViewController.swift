018526d246191154c96560685601005701c7b924
Pavlomat <pavlov_matthew@mail.ru>
2022-07-23 12:13:34 +0300

    Initial Commit

---
+++ MVVM Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Project
+//
+//  Created by Pavlov Matvey on 23.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
